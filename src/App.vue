cad7814cd98b3defc6ced928e75484f56ee96eea
Obidjon4554 <obidjonsamigov@gmail.com>
2023-04-11 17:41:53 +0500

    first repo obidjon

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,7 @@ export default {
 	components: { TheNavbar, }
 }
 </script>
+<!-- 
+git config user.name "Obidjon4554"
+PS C:\Users\User\Desktop\bima-insurance> git config user.email "obidjoninha@gmail.com"
+PS C:\Users\User\Desktop\bima-insurance> git push -u origin obidjon -->
