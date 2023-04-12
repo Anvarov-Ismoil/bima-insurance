417d16359a21df8535249c58cc9448eda2d47534
Obidjon4554 <obidjonsamigov@gmail.com>
2023-04-12 21:04:59 +0500

    repo 4

--- src/sections/Services.vue
+++ src/sections/Services.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="services my-section">
         <div class="container">
-            <TheTitle class="font-extrabold text-4xl text-black/80 pb-8">Популярные услуги</TheTitle>
+            <TheTitle class="font-extrabold text-xl h-a  md:text-4xl text-[#312B2B] pb-10">Популярные услуги</TheTitle>
             <div class="wrapper flex items-center justify-center flex-wrap gap-8 w-full">
-                <TheCard v-for="item in 4" :key="item"><TheButton class="pt-6" type ="bg-brand-color/90" color="text-black" @action="someMethod">Подробнее</TheButton></TheCard>
+                <TheCard v-for="item in 4" :key="item"><TheButton class="pt-6" type ="bg-brand-color/90" color="text-[#312B2B]" @action="someMethod">Подробнее</TheButton></TheCard>
             </div>
         </div>
     </section>
---
+++ src/sections/news.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="news">
+        <div class="container">
+
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
