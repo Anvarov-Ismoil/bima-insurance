aebaad4cf38970f850076da0a6392c842bdd17fd
Ismoil Anvarov <ismoilanvarov26@gmail.com>
2023-04-26 22:03:07 +0500

    fixed and added a lot of things

--- src/sections/Info.vue
+++ src/sections/Info.vue
@@ -1,102 +1,43 @@
 <template>
-  <section class="partners">
+  <section class="info">
     <div class="container">
-      <div class="card">
-        <div class="info flex justify-between py-20 items-center">
-          <div class="flex gap-8 items-center">
-            <h1 class="text-xl md:text-4xl font-extrabold text-dark-gray">
-              О компании в цифрах
-            </h1>
-            <img src="../assets/images/logo.png" alt="#" />
-            <p class="md:max-lg:text-start">
+      <!-- top -->
+      <div class="top flex justify-between py-20 items-center">
+        <div class="flex items-center gap-8">
+          <TheTitle>О компании в цифрах</TheTitle>
+          <div class="center flex items-center gap-8 pb-10">
+            <img src="../assets/images/logo_black.png" alt="#" />
+            <h2 class="text-xl">
               Год основания:
-              <span class="text-[15px] text-brand-color mt-8 px-2">2014</span>
-            </p>
-          </div>
-          <div>
-            <button
-              class="btn py-2 md:py-3 px-6 md:px-7 rounded-[100px] shadow-md border-none transition-all cursor-pointer text-center hover:scale-110 bg-brand-color text-[#312B2B]"
-            >
-              2019 год <i class="fas fa-chevron-down text-[10px]"></i>
-            </button>
+              <span class="text-2xl text-brand-color mt-8 px-2">2014</span>
+            </h2>
           </div>
         </div>
-        <div
-          class="flex justify-between flex-col align-center, sm:flex align-center, xl:flex"
-        >
-          <div
-            class="grid gap-4 sm:grid-cols-2 content-center, grid-cols-1 content-center"
-          >
-            <div class="info">
-              <div class="flex gap-5">
-                <div class="flex">
-                  <img src="../assets/images/icon/Vector.png" alt="#" />
-                  <img src="../assets/images/icon/Vector (1).png" alt="" />
-                </div>
-                <div>
-                  <h1 class="text-[#514A4A]">32 619</h1>
-                  <p>человек застраховано</p>
-                </div>
+        <div class="pb-10">
+          <TheButton type="bg-brand-color" color="text-dark-grey">
+            2019 год <i class="fas fa-chevron-down text-[10px]"></i>
+          </TheButton>
+        </div>
+      </div>
+      <!-- bottom -->
+      <div class="bottom flex justify-between align-center">
+        <div class="w-[50%] flex flex-wrap items-center justify-evenly">
+          <div class="item w-[250px]" v-for="item in infoArray" :key="item">
+            <div class="flex items-center justify-between gap-5">
+              <div class="image-block flex items-center justify-center 
+                w-[60px] h-[60px]">
+                <img :src="item.image" alt="/#/" class="object-cover">
               </div>
-            </div>
-            <div class="info">
-              <div class="flex gap-5">
-                <div class="flex">
-                  <img src="../assets/images/icon/Vector (4).png" alt="#" />
-                </div>
-                <div>
-                  <h1 class="text-[#514A4A]">743</h1>
-                  <p>машин застраховано</p>
-                </div>
-              </div>
-            </div>
-            <div class="info">
-              <div class="flex gap-5">
-                <div class="flex">
-                  <img src="../assets/images/icon/Vector (2).png" alt="" />
-                </div>
-                <div>
-                  <h1 class="text-[#514A4A]">7 462 500 сомони</h1>
-                  <p>выплачено по страховым случаям</p>
-                </div>
-              </div>
-            </div>
-            <div class="info">
-              <div class="flex gap-5">
-                <div class="flex">
-                  <img src="../assets/images/icon/Group.png" alt="" />
-                </div>
-                <div>
-                  <h1 class="text-[#514A4A]">51 250 597 сомони</h1>
-                  <p>страховой премии получено</p>
-                </div>
-              </div>
-            </div>
-            <div class="info">
-              <div class="flex gap-5">
-                <div class="flex">
-                  <img src="../assets/images/icon/Vector (3).png" alt="" />
-                </div>
-                <div>
-                  <h1 class="text-[#514A4A]">11 392</h1>
-                  <p>выезжающих за рубеж застраховано</p>
-                </div>
-              </div>
-            </div>
-            <div class="info">
-              <div class="flex gap-5">
-                <div class="flex">
-                  <img src="../assets/images/icon/Group (1).png" alt="" />
-                </div>
-                <div>
-                  <h1 class="text-[#514A4A]">50 000</h1>
-                  <p>полисов выпущено</p>
-                </div>
+              <div class="text-left">
+                <h1 class="text-dark-gray text-[18px] font-extrabold">{{ item.number }}</h1>
+                <p class="text-[#8D8D8D] text-base">{{ item.text }}</p>
               </div>
             </div>
           </div>
-          <div class="map">
-            <img src="../assets/images/icon/map.jpg" alt="" />
+        </div>
+        <div class="w-[50%] map">
+          <div class="image-block flex justify-end">
+            <img src="../assets/images/xarita.png" alt="" />
           </div>
         </div>
       </div>
@@ -105,10 +46,53 @@
 </template>
 
 <script>
+import TheTitle from '../components/TheTitle.vue';
+import TheButton from "../components/TheButton.vue";
+import image_1 from '../assets/icons/employees.png';
+import image_2 from '../assets/icons/car.png'
+import image_3 from '../assets/icons/money.png'
+import image_4 from '../assets/icons/safe.png'
+import image_5 from '../assets/icons/case.png'
+import image_6 from '../assets/icons/file.png'
+
 export default {
   data() {
-    return {};
+    return {
+      infoArray: [
+        {
+          image: image_1,
+          number: '32 619',
+          text: 'человек застраховано',
+        },
+        {
+          image: image_2,
+          number: '743',
+          text: 'машин застраховано',
+        },
+        {
+          image: image_3,
+          number: '7 462 500 сомони',
+          text: 'выплачено по страховым случаям',
+        },
+        {
+          image: image_4,
+          number: '51 250 597 сомони',
+          text: 'страховой премии получено',
+        },
+        {
+          image: image_5,
+          number: '11 392',
+          text: 'выезжающих за рубеж застраховано',
+        },
+        {
+          image: image_6,
+          number: '50 000',
+          text: 'полисов выпущено',
+        },
+      ]
+    };
   },
+  components: { TheTitle, TheButton }
 };
 </script>
 
